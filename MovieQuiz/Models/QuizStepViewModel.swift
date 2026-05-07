125b46c8e2d6522ccb91eed673db035c122506a8
dobroskyy <dobrozhinsky@gmail.com>
2026-05-07 22:15:16 +0300

    Реализация UI и логики

---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Максим on 02.05.2026.
+//
+
+import UIKit
+
+struct QuizStepViewModel {
+  let image: UIImage
+  let question: String
+  let questionNumber: String
+}
